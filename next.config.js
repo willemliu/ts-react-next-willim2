53f43261c88327c43da076d4e2d17fb0fa2868ae
Willem Liu <willem_liu@hotmail.com>
2019-05-02 12:47:41 +0200

    Updated dependencies

--- next.config.js
+++ next.config.js
@@ -1,14 +1,7 @@
 const withTypescript = require('@zeit/next-typescript')
 const {PHASE_DEVELOPMENT_SERVER} = require('next/constants');
 
-module.exports = withTypescript((phase, {defaultConfig}) => {
-    if(phase === PHASE_DEVELOPMENT_SERVER) {
-        return {
-          /* development only config options here */
-        }
-    }
-    
-    return {
-        pageExtensions: ['jsx', 'js', 'ts', 'tsx']
-    }    
+module.exports = withTypescript({
+    // target: 'serverless',
+    pageExtensions: ['jsx', 'js', 'ts', 'tsx']
 });
