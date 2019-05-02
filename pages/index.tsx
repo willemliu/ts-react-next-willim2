85ab0aeb34583c9b8db0667786e0733500e14426
Willem Liu <willem_liu@hotmail.com>
2019-05-02 13:23:58 +0200

    Added favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,6 +11,7 @@ export default class Index extends PureComponent<any, any> {
                     <title>WilliM - Vanilla web-development</title>
                     <meta name="viewport" content="initial-scale=1.0, width=device-width" />
                     <meta name="description" content="Vanilla web-development"/>
+                    <link rel="icon" type="image/x-icon" href="/static/img/favicon.ico"/>
                     <script type="text/javascript" dangerouslySetInnerHTML={{__html: `var appInsights=window.appInsights||function(a){ function b(a){c[a]=function(){var b=arguments;c.queue.push(function(){c[a].apply(c,b)})}}var c={config:a},d=document,e=window;setTimeout(function(){var b=d.createElement("script");b.src=a.url||"https://az416426.vo.msecnd.net/scripts/a/ai.0.js",d.getElementsByTagName("script")[0].parentNode.appendChild(b)});try{c.cookie=d.cookie}catch(a){}c.queue=[];for(var f=["Event","Exception","Metric","PageView","Trace","Dependency"];f.length;)b("track"+f.pop());if(b("setAuthenticatedUserContext"),b("clearAuthenticatedUserContext"),b("startTrackEvent"),b("stopTrackEvent"),b("startTrackPage"),b("stopTrackPage"),b("flush"),!a.disableExceptionTracking){f="onerror",b("_"+f);var g=e[f];e[f]=function(a,b,d,e,h){var i=g&&g(a,b,d,e,h);return!0!==i&&c["_"+f](a,b,d,e,h),i}}return c }({ instrumentationKey:"29bb20e8-d77a-492d-8d1c-5c21ee8ce167" }); window.appInsights=appInsights,appInsights.queue&&0===appInsights.queue.length&&appInsights.trackPageView();`}}/>
                 </Head>
                 <div className="body">
